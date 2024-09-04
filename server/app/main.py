55cc018689fd4dba577ed41bf187756ccb69f2c0
Dirk Doesburg <dirk.doesburg@gmail.com>
2024-09-04 11:56:48 +0200

    Prepare for deployment with proxy
    I'm not actually using this nginx image when deploying at home,
    because I need a TLS-terminating reverse proxy that's shared with other projects.

--- server/app/main.py
+++ server/app/main.py
@@ -7,9 +7,12 @@ from app.exchanges.api import router as exchanges_router
 app = FastAPI(
     title="DIYivi",
     summary="Backend for DIYivi, a DIY tool for exchanging Yivi attributes.",
+    openapi_url="/api/openapi.json",
+    docs_url="/api/docs",
+    redoc_url=None,
 )
 
-app.include_router(exchanges_router, prefix="/exchanges")
+app.include_router(exchanges_router, prefix="/api/exchanges")
 
 
 def output_schema():
