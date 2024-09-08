4c4219b609e74db803b1c3a32671475c703685a0
Dirk Doesburg <dirk.doesburg@gmail.com>
2024-09-08 20:08:57 +0200

    Add logic to actually make API call

--- client/src/views/ExchangeStartView.vue
+++ client/src/views/ExchangeStartView.vue
@@ -1,36 +1,69 @@
 <script setup lang="ts">
+import client from '@/api'
 import { Checkbox } from '@/components/ui/checkbox'
+import { Button } from '@/components/ui/button'
 import { Label } from '@/components/ui/label'
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
+import { ref } from 'vue'
 
-const publicAttributeOptions = {
+const publicAttributeOptions: {
+  [key: string]: { label: string; description: string | null; attributeId: string }
+} = {
   mobilenumber: {
     label: 'Mobiel telefoonnumer',
-    description: 'Bijvoorbeeld via WhatsApp of Signal.'
+    description: 'Bijvoorbeeld via WhatsApp of Signal.',
+    attributeId: 'irma-demo.sidn-pbdf.mobilenumber.mobilenumber'
   },
   email: {
     label: 'E-mailadres',
-    description: null
+    description: null,
+    attributeId: 'irma-demo.sidn-pbdf.email.email'
   }
 }
 
-const attributeOptions = {
+const attributeOptions: {
+  [key: string]: { label: string; attributeId: string }
+} = {
   name: {
     label: 'Naam',
-    description: null
+    attributeId: 'irma-demo.gemeente.personalData.fullname'
   },
   birthdate: {
     label: 'Geboortedatum',
-    description: null
+    attributeId: 'irma-demo.sidn-pbdf.personalData.dateofbirth'
   },
   mobilenumber: {
     label: 'Mobiel telefoonnumer',
-    description: null
+    attributeId: 'irma-demo.sidn-pbdf.mobilenumber.mobilenumber'
   },
   email: {
     label: 'E-mailadres',
-    description: null
+    attributeId: 'irma-demo.sidn-pbdf.email.email'
   }
+}
+
+const publicAttribute = ref('mobilenumber')
+const selectedAttributes = ref(new Set<string>())
+
+function setSelectedAttribute(attribute: string, checked: boolean) {
+  console.log(attribute, checked)
+  if (checked) selectedAttributes.value.add(attribute)
+  else selectedAttributes.value.delete(attribute)
+}
+
+async function createExchange() {
+  console.log(publicAttribute.value)
+
+  const response = await client.POST('/api/exchanges/create/', {
+    body: {
+      attributes: [
+        [[...selectedAttributes.value].map((attribute) => attributeOptions[attribute]!.attributeId)]
+      ],
+      public_initiator_attributes: [[publicAttributeOptions[publicAttribute.value]!.attributeId]]
+    }
+  })
+
+  console.log(response)
 }
 </script>
 <template>
@@ -47,13 +80,13 @@ const attributeOptions = {
         Deze informatie krijgt de ontvanger al te zien voordat diegene besluit gegevens te delen. Zo
         weet de ontvanger zeker dat jij degene bent die de link heeft gestuurd.
       </p>
-      <RadioGroup class="pt-4 pb-2">
+      <RadioGroup v-model="publicAttribute" class="pt-4 pb-2">
         <div
           v-for="({ label, description }, attribute) in publicAttributeOptions"
           class="flex items-start space-x-2"
           :key="attribute"
         >
-          <RadioGroupItem :id="`public-attribute-${attribute}`" :value="attribute" />
+          <RadioGroupItem :id="`public-attribute-${attribute}`" :value="attribute as string" />
           <Label :for="`public-attribute-${attribute}`">
             {{ label }}
             <p v-if="description" class="font-normal text-xs text-muted-foreground py-1">
@@ -72,19 +105,21 @@ const attributeOptions = {
       </p>
       <div class="grid gap-2 pt-4 pb-2">
         <div
-          v-for="({ label, description }, attribute) in attributeOptions"
+          v-for="({ label }, attribute) of attributeOptions"
           :key="attribute"
           class="flex items-start space-x-2"
         >
-          <Checkbox :id="`attribute-${attribute}`" />
+          <Checkbox
+            :id="`attribute-${attribute}`"
+            @update:checked="(checked) => setSelectedAttribute(attribute as string, checked)"
+          />
           <Label :for="`attribute-${attribute}`">
             {{ label }}
-            <p v-if="description" class="font-normal text-xs text-muted-foreground py-1">
-              {{ description }}
-            </p>
           </Label>
         </div>
       </div>
     </div>
+
+    <Button @click="createExchange" class="pt-4"> Aanmaken </Button>
   </div>
 </template>
