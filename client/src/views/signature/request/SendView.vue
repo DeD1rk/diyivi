24579a9b051aeb7f7253951542549fc13ffc549b
Dirk Doesburg <dirk.doesburg@gmail.com>
2024-11-15 13:45:20 +0100

    Clean up frontend a bit

--- client/src/views/signature/request/SendView.vue
+++ client/src/views/signature/request/SendView.vue
@@ -1,20 +1,17 @@
 <script setup lang="ts">
-import client from '@/api'
-import Title from '@/components/Title.vue'
 import Header from '@/components/Header.vue'
-import { Copy, Mail } from 'lucide-vue-next'
+import { Mail } from 'lucide-vue-next'
 import { useToast } from '@/components/ui/toast'
 import WhatsApp from '@/components/icons/WhatsApp.vue'
 import { Button } from '@/components/ui/button'
-import { useTimeoutPoll } from '@vueuse/core'
 import { computed } from 'vue'
 import type { SignatureRequestResponse } from '@/api/types'
+import RawLinkDisplay from '@/components/RawLinkDisplay.vue'
 
 const props = defineProps<{
   request: SignatureRequestResponse
 }>()
 
-const { toast } = useToast()
 const respondUrl = computed(() => `${window.origin}/signature/request/respond/${props.request.id}/`)
 
 const mailtoUrl = computed(() => {
@@ -32,12 +29,6 @@ const whatsappUrl = computed(() => {
 
   return `whatsapp://send?text=${encodeURIComponent(content + respondUrl.value)}`
 })
-async function copyLink() {
-  await navigator.clipboard.writeText(respondUrl.value)
-  toast({
-    description: 'Je uitnodiging is gekopieerd.'
-  })
-}
 </script>
 <template>
   <Header>Stap 4: Verstuur de link</Header>
@@ -46,12 +37,7 @@ async function copyLink() {
     de onderstaande link openen en de instructies volgen. Als de ontvanger de afspraak ondertekend
     heeft ontvang je een e-mail met de handtekening.
   </p>
-  <div class="font-mono bg-yivi-lightblue py-2 px-4 mt-4 break-all select-all">
-    {{ respondUrl
-    }}<button @click="copyLink">
-      <Copy class="inline ms-2 w-4 h-4 hover:scale-110 transition" />
-    </button>
-  </div>
+  <RawLinkDisplay :link="respondUrl" copyMessage="Je uitnodiging is gekopieerd." class="mt-4" />
   <div id="share-buttons" class="mt-4 flex gap-2">
     <Button as-child>
       <a :href="whatsappUrl"><WhatsApp class="w-4 h-4 me-2" />Verstuur met WhatsApp</a>
